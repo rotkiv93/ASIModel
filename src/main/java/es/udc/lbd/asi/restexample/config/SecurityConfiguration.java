61292d96977da6f2c9051984afef06826f2e12d9
Pablo <pablestal@gmail.com>
2018-10-31 17:41:45 +0100

    El puto access denied

--- src/main/java/es/udc/lbd/asi/restexample/config/SecurityConfiguration.java
+++ src/main/java/es/udc/lbd/asi/restexample/config/SecurityConfiguration.java
@@ -79,6 +79,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .antMatchers("/api/authenticate").permitAll()
             .antMatchers(HttpMethod.POST, "/api/register").permitAll()
             .antMatchers(HttpMethod.GET, "/api/posts/**").permitAll()
+            .antMatchers(HttpMethod.GET, "/api/movies/**").permitAll()
             .antMatchers("/**").authenticated()
         .and()
             .apply(securityConfigurerAdapter());;
